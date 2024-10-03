7c98b138e74b4d24df20aae618d048aa4831a9dc
resqiar <resqiagengr@gmail.com>
2024-10-03 17:04:45 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "8080"
+	}
+
+	if err := app.Listen(fmt.Sprintf(":%s", PORT)); err != nil {
+		log.Fatalf("Failed to serve the server at port: %s", PORT)
+	}
+}
