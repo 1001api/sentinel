37014a162a57de9ccbfe6fa194d75cb26b87a8bf
resqiar <resqiagengr@gmail.com>
2024-10-08 13:43:18 +0700

    feat: mockup for login page

--- routes/web.route.go
+++ routes/web.route.go
@@ -8,5 +8,9 @@ import (
 
 func InitWebRoute(app *fiber.App, m middlewares.Middleware, webService services.WebService) {
 	app.Get("/", webService.SendLandingPage)
+	app.Get("/login", webService.SendLoginPage)
 	app.Get("/dashboard", m.ProtectedRoute, webService.SendLandingPage)
+
+	misc := app.Group("/misc")
+	misc.Get("/tos", webService.SendTOSPage)
 }
--- services/web.service.go
+++ services/web.service.go
@@ -4,11 +4,14 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/hubkudev/sentinel/configs"
 	"github.com/hubkudev/sentinel/views/pages"
+	"github.com/hubkudev/sentinel/views/pages/misc"
 )
 
 type WebService interface {
 	SendLandingPage(ctx *fiber.Ctx) error
+	SendLoginPage(ctx *fiber.Ctx) error
 	SendDashboardPage(ctx *fiber.Ctx) error
+	SendTOSPage(ctx *fiber.Ctx) error
 }
 
 type WebServiceImpl struct{}
@@ -17,6 +20,14 @@ func (s *WebServiceImpl) SendLandingPage(c *fiber.Ctx) error {
 	return configs.Render(c, pages.IndexPage())
 }
 
+func (s *WebServiceImpl) SendLoginPage(c *fiber.Ctx) error {
+	return configs.Render(c, pages.LoginPage())
+}
+
 func (s *WebServiceImpl) SendDashboardPage(c *fiber.Ctx) error {
 	return configs.Render(c, pages.DashboardPage())
+}
+
+func (s *WebServiceImpl) SendTOSPage(c *fiber.Ctx) error {
+	return configs.Render(c, misc.TOSPage())
 }
