9252fd4e4ffc6499383cb7e3e7183289af4cf87f
resqiar <resqiagengr@gmail.com>
2024-11-12 14:54:04 +0700

    test: complete unit test for userservice

--- internal/services/user_service_test.go
+++ internal/services/user_service_test.go
@@ -21,8 +21,6 @@ func initTest(t *testing.T) (*mocks.UserRepo, UserServiceImpl) {
 }
 
 func TestFindUserByEmail(t *testing.T) {
-	mockUserRepo, userService := initTest(t)
-
 	tests := []struct {
 		name           string
 		email          string
@@ -49,6 +47,8 @@ func TestFindUserByEmail(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			mockUserRepo, userService := initTest(t)
+
 			var mockReturn gen.FindUserByEmailRow
 			if test.expectedResult != nil {
 				mockReturn = gen.FindUserByEmailRow{
@@ -78,8 +78,6 @@ func TestFindUserByEmail(t *testing.T) {
 }
 
 func TestFindUserByEmailWithHash(t *testing.T) {
-	mockUserRepo, userService := initTest(t)
-
 	tests := []struct {
 		name           string
 		email          string
@@ -106,6 +104,8 @@ func TestFindUserByEmailWithHash(t *testing.T) {
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			mockUserRepo, userService := initTest(t)
+
 			var mockReturn gen.FindUserByEmailWithHashRow
 			if test.expectedResult != nil {
 				mockReturn = gen.FindUserByEmailWithHashRow{
@@ -133,3 +133,288 @@ func TestFindUserByEmailWithHash(t *testing.T) {
 		})
 	}
 }
+
+func TestFindUserByID(t *testing.T) {
+	tests := []struct {
+		name           string
+		id             uuid.UUID
+		expectedResult *gen.FindUserByIDRow
+		expectedError  error
+	}{
+		{
+			name: "Should return user account if ID exist",
+			id:   uuid.MustParse("ef687f52-2c0f-4ba4-ad49-7bfe5df8977b"),
+			expectedResult: &gen.FindUserByIDRow{
+				ID:       uuid.MustParse("ef687f52-2c0f-4ba4-ad49-7bfe5df8977b"),
+				Fullname: faker.Name(),
+				Email:    faker.Email(),
+			},
+			expectedError: nil,
+		},
+		{
+			name:           "Should return error if ID does not exist",
+			id:             uuid.MustParse("491d660e-5b7c-4838-9d64-ca70eda51e18"),
+			expectedResult: nil,
+			expectedError:  errors.New("user not found"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			mockUserRepo, userService := initTest(t)
+
+			var mockReturn gen.FindUserByIDRow
+			if test.expectedResult != nil {
+				mockReturn = gen.FindUserByIDRow{
+					ID:       test.expectedResult.ID,
+					Fullname: test.expectedResult.Fullname,
+					Email:    test.expectedResult.Email,
+				}
+			}
+
+			// mock repo call
+			mockUserRepo.Mock.On(
+				"FindUserByID",
+				context.Background(),
+				test.id,
+			).Return(mockReturn, test.expectedError)
+
+			result, err := userService.FindByID(test.id)
+
+			if test.expectedError != nil {
+				assert.Nil(t, result)
+				assert.NotNil(t, err)
+				assert.Error(t, err)
+			} else {
+				assert.Nil(t, err)
+				assert.NotEmpty(t, result)
+				assert.IsType(t, &gen.FindUserByIDRow{}, result)
+				assert.Equal(t, test.expectedResult, result)
+			}
+		})
+	}
+}
+
+func TestFindByPublicKey(t *testing.T) {
+	tests := []struct {
+		name           string
+		key            string
+		expectedResult *gen.FindUserByPublicKeyRow
+		expectedError  error
+	}{
+		{
+			name: "Should return user account if key exist",
+			key:  faker.Password(),
+			expectedResult: &gen.FindUserByPublicKeyRow{
+				ID:       uuid.MustParse(faker.UUIDHyphenated()),
+				Fullname: faker.Name(),
+				Email:    faker.Email(),
+			},
+			expectedError: nil,
+		},
+		{
+			name:           "Should return error if key does not exist",
+			key:            faker.Password(),
+			expectedResult: nil,
+			expectedError:  errors.New("user not found"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			mockUserRepo, userService := initTest(t)
+
+			var mockReturn gen.FindUserByPublicKeyRow
+			if test.expectedResult != nil {
+				mockReturn = gen.FindUserByPublicKeyRow{
+					ID:       test.expectedResult.ID,
+					Fullname: test.expectedResult.Fullname,
+					Email:    test.expectedResult.Email,
+				}
+			}
+
+			// mock repo call
+			mockUserRepo.Mock.On(
+				"FindUserByPublicKey",
+				context.Background(),
+				test.key,
+			).Return(mockReturn, test.expectedError)
+
+			result, err := userService.FindByPublicKey(test.key)
+
+			if test.expectedError != nil {
+				assert.Nil(t, result)
+				assert.NotNil(t, err)
+				assert.Error(t, err)
+			} else {
+				assert.Nil(t, err)
+				assert.NotEmpty(t, result)
+				assert.IsType(t, &gen.FindUserByPublicKeyRow{}, result)
+				assert.Equal(t, test.expectedResult, result)
+			}
+		})
+	}
+}
+
+func TestGetPublicKey(t *testing.T) {
+	tests := []struct {
+		name           string
+		userID         uuid.UUID
+		expectedResult string
+		expectedError  error
+	}{
+		{
+			name:           "Should return user public key if user exist",
+			userID:         uuid.MustParse(faker.UUIDHyphenated()),
+			expectedResult: faker.Password(),
+			expectedError:  nil,
+		},
+		{
+			name:           "Should return error if user does not exist",
+			userID:         uuid.MustParse(faker.UUIDHyphenated()),
+			expectedResult: "",
+			expectedError:  errors.New("user not found"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			mockUserRepo, userService := initTest(t)
+
+			// mock repo call
+			mockUserRepo.Mock.On(
+				"FindUserPublicKey",
+				context.Background(),
+				test.userID,
+			).Return(test.expectedResult, test.expectedError)
+
+			result, err := userService.GetPublicKey(test.userID)
+
+			if test.expectedError != nil {
+				assert.Empty(t, result)
+				assert.NotNil(t, err)
+				assert.Error(t, err)
+			} else {
+				assert.Nil(t, err)
+				assert.NotEmpty(t, result)
+				assert.IsType(t, "", result)
+				assert.Equal(t, test.expectedResult, result)
+			}
+		})
+	}
+}
+
+func TestCheckAdminExist(t *testing.T) {
+	tests := []struct {
+		name           string
+		expectedResult bool
+		expectedError  error
+	}{
+		{
+			name:           "Should return true if admin user exist",
+			expectedResult: true,
+			expectedError:  nil,
+		},
+		{
+			name:           "Should return false if admin user does not exist",
+			expectedResult: false,
+			expectedError:  errors.New("user not found"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			mockUserRepo, userService := initTest(t)
+
+			// mock repo call
+			mockUserRepo.Mock.On(
+				"CheckAdminExist",
+				context.Background(),
+			).Return(test.expectedResult, test.expectedError)
+
+			result, err := userService.CheckAdminExist()
+
+			if test.expectedError != nil {
+				assert.False(t, result)
+				assert.NotNil(t, err)
+				assert.Error(t, err)
+			} else {
+				assert.Nil(t, err)
+				assert.True(t, result)
+				assert.IsType(t, true, result)
+				assert.Equal(t, test.expectedResult, result)
+			}
+		})
+	}
+
+}
+
+func TestCreateUser(t *testing.T) {
+	expectedFullname := faker.Name()
+	expectedEmail := faker.Email()
+
+	tests := []struct {
+		name           string
+		payload        *gen.CreateUserParams
+		expectedResult *gen.CreateUserRow
+		expectedError  error
+	}{
+		{
+			name: "Should return true if admin user exist",
+			payload: &gen.CreateUserParams{
+				Fullname: expectedFullname,
+				Email:    expectedEmail,
+			},
+			expectedResult: &gen.CreateUserRow{
+				Fullname: expectedFullname,
+				Email:    expectedEmail,
+			},
+			expectedError: nil,
+		},
+		{
+			name: "Should return false if admin user does not exist",
+			payload: &gen.CreateUserParams{
+				Fullname: expectedFullname,
+				Email:    expectedEmail,
+			},
+			expectedResult: nil,
+			expectedError:  errors.New("something bad happen"),
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			mockUserRepo, userService := initTest(t)
+
+			var mockReturn gen.CreateUserRow
+			if test.expectedResult != nil {
+				mockReturn = gen.CreateUserRow{
+					Fullname: test.expectedResult.Fullname,
+					Email:    test.expectedResult.Email,
+				}
+			}
+
+			// mock repo call
+			mockUserRepo.Mock.On(
+				"CreateUser",
+				context.Background(),
+				test.payload,
+			).Return(mockReturn, test.expectedError)
+
+			result, err := userService.CreateUser(test.payload)
+
+			if test.expectedError != nil {
+				assert.Nil(t, result)
+				assert.NotNil(t, err)
+				assert.Error(t, err)
+			} else {
+				assert.Nil(t, err)
+				assert.NotNil(t, result)
+				assert.IsType(t, &gen.CreateUserRow{}, result)
+				assert.Equal(t, test.expectedResult, result)
+			}
+		})
+	}
+
+}
+
